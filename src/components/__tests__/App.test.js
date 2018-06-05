34ca55dd0b93d6cad50bff9c66a8aa5af671d63e
Luis Locon <luis.locon@gmail.com>
2018-06-04 23:43:22 -0600

    adding enzyme to our project

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
+import CommentBox from '../CommentBox'
 
 it('Shows a comment box', () => {
-  const div = document.createElement('div')
-
-  ReactDOM.render(<App />, div)
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
-  // console.log('div', div.innerHTML) output <div><div>CommentBox</div><div>Comment List</div></div>
-  expect(div.innerHTML).toContain('CommentBox')
-
+  const wrapped = shallow(<App />)
+  expect(wrapped.find(CommentBox).length).toEqual(1)
 
   // Line below uses for cleaning up the component mounted 
-  ReactDOM.unmountComponentAtNode(div)
+  // ReactDOM.unmountComponentAtNode(div)
 })
