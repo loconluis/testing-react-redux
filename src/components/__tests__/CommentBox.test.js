76a0f744f3d19bc418b60fa052249c49534213c4
Luis Locon <luis.locon@gmail.com>
2018-06-05 00:49:43 -0600

    testing if textarea receive the new value from state.

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,11 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1)
   expect(wrapped.find('button').length).toEqual(1)
 })
+
+it('textarea support handle change', () => {
+  let value = 'Trying textArea'
+  wrapped.find('textarea').simulate('change', { target: { value } })
+  wrapped.update()
+  expect(wrapped.find('textarea').prop('value')).toEqual(value)
+  // expect(wrapped.state('comment')).toBe(value)
+})
