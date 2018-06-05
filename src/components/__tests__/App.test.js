7baff0e613fd2cadb8a6e64e9adf2cb01dcccd67
Luis Locon <luis.locon@gmail.com>
2018-06-04 23:55:18 -0600

    creating a file called .env to relative paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from '../App'
-import CommentBox from '../CommentBox'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
 
 it('Shows a comment box', () => {
   const wrapped = shallow(<App />)
   expect(wrapped.find(CommentBox).length).toEqual(1)
+
+  // Line below uses for cleaning up the component mounted 
+  // ReactDOM.unmountComponentAtNode(div)
+})
+
+it('Shows a comment list', () => {
+  const wrapped = shallow(<App />)
+  expect(wrapped.find(CommentList).length).toEqual(1)
 
   // Line below uses for cleaning up the component mounted 
   // ReactDOM.unmountComponentAtNode(div)
