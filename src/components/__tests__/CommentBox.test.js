583c0d75d7324b98e5a276a0f8df995bc7af73c6
Luis Locon <luis.locon@gmail.com>
2018-06-05 01:04:18 -0600

    Describe statement

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,10 +17,24 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('textarea support handle change', () => {
-  let value = 'Trying textArea'
-  wrapped.find('textarea').simulate('change', { target: { value } })
-  wrapped.update()
-  expect(wrapped.find('textarea').prop('value')).toEqual(value)
-  // expect(wrapped.state('comment')).toBe(value)
+// Delimitate scope 
+describe('Handle events changing textarea value', () => {
+  let value
+  beforeEach(() => {
+    value = 'Trying textArea'
+    wrapped.find('textarea').simulate('change', { target: { value } })
+    wrapped.update()
+  })
+
+  it('textarea support handle change', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual(value)
+    // expect(wrapped.state('comment')).toBe(value)
+  })
+  
+  it('textarea cleaning when form is submitted', () => {
+    wrapped.find('form').simulate('submit', { preventDefault: () => {} })
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+    expect(wrapped.state('comment')).toBe('')
+  })
 })
